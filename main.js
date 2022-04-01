a2082fdcc4d7465f9e3d11c115abcc5f8e8852a8
Ambar Cano <ambarcano@gmail.com>
2022-03-31 21:27:04 -0400

    2nd update

---
+++ main.js
@@ -0,0 +1,61 @@
+
+// let bnum1 = document.getElementById('num1')
+// bnum1.addEventListener('click', function() { 
+//     console.log(1)})
+
+// let bnum1 = document.getElementById('num2')
+// bnum1.addEventListener('click', function() { 
+//     console.log(1)})
+
+let bclear = document.querySelector('#opC')
+bclear.addEventListener('click', function() { 
+    console.log('')})
+
+let bnum0= document.querySelector('#num0')
+bnum0.addEventListener('click', function() { 
+    console.log(0)})
+
+let bnum1= document.querySelector('#num1')
+bnum1.addEventListener('click', function() { 
+    console.log(1)})
+
+ let bnum2 = document.querySelector('#num2')
+bnum2.addEventListener('click', function() { 
+    console.log(2)})
+
+let bnum3 = document.querySelector('#num3')
+bnum3.addEventListener('click', function() { 
+    console.log(3)})
+
+let bnum4= document.querySelector('#num4')
+bnum4.addEventListener('click', function() { 
+    console.log(4)})
+
+let bnum5 = document.querySelector('#num5')
+bnum5.addEventListener('click', function() { 
+    console.log(5)})
+
+let bnum6 = document.querySelector('#num6')
+bnum6.addEventListener('click', function() { 
+    console.log(6)})
+
+let bnum7= document.querySelector('#num7')
+bnum7.addEventListener('click', function() { 
+    console.log(7)})
+
+let bnum8 = document.querySelector('#num8')
+bnum8.addEventListener('click', function() { 
+    console.log(8)})
+
+let bnum9= document.querySelector('#num9')
+bnum9.addEventListener('click', function() { 
+    console.log(9)})
+
+let bopdiv= document.querySelector('#opdiv')
+bopdiv.addEventListener('click', function() { 
+    console.log('/')})
+
+let bopmult= document.querySelector('#opmult')
+bopmult.addEventListener('click', function() { 
+    console.log(*)})
+
