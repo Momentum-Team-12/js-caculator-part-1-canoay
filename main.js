812d07ee693659333beaef18d4bf078713989f1f
Ambar Cano <ambarcano@gmail.com>
2022-04-04 09:04:16 -0400

    buttons work

--- main.js
+++ main.js
@@ -1,69 +1,19 @@
 
-let bclear = document.querySelector('#opC')
-bclear.addEventListener('click', function() { 
-    console.log('')})
+let display = document.querySelector("#screen")
+let results = document.querySelectorAll("#equal")
+let buttons = document.querySelectorAll(".button");
+let clear = document.querySelectorAll("#opC")
 
-let bnum0= document.querySelector('#num0')
-bnum0.addEventListener('click', function() { 
-    console.log(0)})
+for (let button of buttons) {
+  button.addEventListener ("click", () => {
+    display.innerText += button.innerText
+  })
+}
 
-let bnum1= document.querySelector('#num1')
-bnum1.addEventListener('click', function() { 
-    console.log(1)})
+clear.addEventListener ('click',  () => {
+    display.innerText = ""
+  })
 
- let bnum2 = document.querySelector('#num2')
-bnum2.addEventListener('click', function() { 
-    console.log(2)})
-
-let bnum3 = document.querySelector('#num3')
-bnum3.addEventListener('click', function() { 
-    console.log(3)})
-
-let bnum4= document.querySelector('#num4')
-bnum4.addEventListener('click', function() { 
-    console.log(4)})
-
-let bnum5 = document.querySelector('#num5')
-bnum5.addEventListener('click', function() { 
-    console.log(5)})
-
-let bnum6 = document.querySelector('#num6')
-bnum6.addEventListener('click', function() { 
-    console.log(6)})
-
-let bnum7= document.querySelector('#num7')
-bnum7.addEventListener('click', function() { 
-    console.log(7)})
-
-let bnum8 = document.querySelector('#num8')
-bnum8.addEventListener('click', function() { 
-    console.log(8)})
-
-let bnum9= document.querySelector('#num9')
-bnum9.addEventListener('click', function() { 
-    console.log(9)})
-
-let bopdiv= document.querySelector('#opdiv')
-bopdiv.addEventListener('click', function() { 
-    console.log('/')})
-
-let bopmult= document.querySelector('#opmult')
-bopmult.addEventListener('click', function() { 
-    console.log('*')})
-
-let bopsub= document.querySelector('#opsub')
-bopsub.addEventListener('click', function() { 
-    console.log('-')})
-
-let bopadd= document.querySelector('#opadd')
-bopadd.addEventListener('click', function() { 
-    console.log('+')})
-
-let bopdec= document.querySelector('#opdec')
-bopdec.addEventListener('click', function() { 
-    console.log('.')})
-
-let bequal= document.querySelector('#equal')
-bequal.addEventListener('click', function() { 
-    console.log('=')})
-
+results.addEventListener ('click',  () => {
+    display.innerText = eval(display.innerText)
+  })
